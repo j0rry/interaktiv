048404024069928a5efd86e9147c2379628fee45
Oscar Alshammar <oscar.alshammar@gmail.com>
2024-09-04 15:00:55 +0200

    📄Interaktiv berättelse

--- Program.cs
+++ Program.cs
@@ -1,25 +1,24 @@
 ﻿
 
-using System.Security.AccessControl;
 
 class Program{
     static void Main(){
         loadingAnimation();
         Random random = new Random();
 
-        bool gameOver = true;
+        bool gameOver = false;
         int money = 0;
         int room = 0;
         string[] roomNames = { "Main Room","Vault Room", "Living Room", "Kitchen"};
-        
+        int vaultCode = random.Next(1000, 9999);
 
-        int vaultCode = 1000;
+
+       // int vaultCode = 1000;
         bool vaultIsRobbed = false;
 
         Console.Clear();
         Console.WriteLine("Klicka SPACE för att starta!");
-        var startKey = Console.ReadKey(intercept: true);
-        if(startKey.Key == ConsoleKey.Spacebar) gameOver = false;
+        Console.ReadKey();
         
         while(!gameOver) { // Kollar om spelet är över
             Console.Clear();
@@ -41,16 +40,42 @@ class Program{
                     }
                     else {
                         Console.WriteLine("Du kliver in i ett rum med ett kassaskåp, kassaskåpet är låst \n");
-                        vaultManager(ref vaultCode, ref room, ref money, random.Next(50, 1000), ref vaultIsRobbed);
+                        vaultManager(ref vaultCode, ref room, ref money, random.Next(50, 1000), ref vaultIsRobbed, ref gameOver);
                     }
                     break;
                 case 2:
-                    Console.WriteLine("Du kliver in i ett rum med en soffa och en TV \n");
-                    roomInput(ref room);
+                    Console.WriteLine("Du kliver in i ett rum med en soffa och en TV \n Du lägger märke på att det sitter en man i soffan. Du ser även att det befinner sig en lapp på andra sidan rummet på väggen. \n");
+                    System.Console.WriteLine("Commands: \n Lapp \n Mannen");
+                    string choice = Console.ReadLine() ?? string.Empty;
+                    if(choice.ToLower() == "lapp") {
+                        System.Console.WriteLine("######");
+                        System.Console.WriteLine($"#{vaultCode}#");
+                        System.Console.WriteLine("######");
+                        System.Console.WriteLine("Denna kod kan vara viktig! Kom ihåg den. \n");
+                        
+                        roomInput(ref room);
+                    }
+                    if(choice.ToLower() == "mannen") {
+                        gameOver = true;
+                        System.Console.WriteLine("Mannen tog fram ett maskingevär och döda dig!");
+                        System.Console.WriteLine("Klicka Space för att börja om");
+                        var inputKey1 = Console.ReadKey(intercept: true);
+                        if(inputKey1.Key == ConsoleKey.Spacebar) {
+                            Main();
+                        }
+                        
+                    }
+
                     break;
                 case 3:
-                    Console.WriteLine("Du kliver in i ett rum med en köksbänk och en spis \n");
-                    roomInput(ref room);
+                    Console.Clear();
+                    Console.WriteLine("Du kliver in i ett kök med en arg kok som dödar dig. \n \n klicka Spacebar för att börja om!");
+                    gameOver = true;
+                    var inputKey2 = Console.ReadKey(intercept: true);
+                        if(inputKey2.Key == ConsoleKey.Spacebar) {
+                            Main();
+                        }
+                    
                     break;
                 case 9:
                     gameOver = true;
@@ -62,13 +87,11 @@ class Program{
 
             }
         }
-
-        Console.WriteLine("Tryck på valfri tangent för att avsluta...");
-        Console.ReadKey();
     }
 
     static void roomInput(ref int room){ // Kollar vilket rum du vill gå till
         while(true){
+            System.Console.WriteLine("Välj ett nummer 1-3 \n");
             System.Console.WriteLine("1. Vault Room");
             System.Console.WriteLine("2. Living Room");
             System.Console.WriteLine("3. Kitchen");
@@ -91,8 +114,9 @@ class Program{
         }
     }
 
-    static void vaultManager(ref int vaultCode, ref int room, ref int money, int vaultMoney, ref bool vault){ // Kollar om kod till kassaskåpet är rätt
+    static void vaultManager(ref int vaultCode, ref int room, ref int money, int vaultMoney, ref bool vault, ref bool gameOver){ // Kollar om kod till kassaskåpet är rätt
         while(true){
+            System.Console.WriteLine("För att lämna rummet skriv Exit");
             System.Console.Write("Skriv in koden: ");
             string input = Console.ReadLine() ?? string.Empty;
             if(input.ToLower() == "exit"){
@@ -103,9 +127,11 @@ class Program{
             {
                 if(kod == vaultCode){
                     money = vaultMoney; 
-                    System.Console.WriteLine($"+{vaultMoney}$ pengar");
-                    Thread.Sleep(500);
                     vault = true;
+                    gameOver = true;
+                    Console.Clear();
+                    System.Console.WriteLine($"Du Lyckades själa {vaultMoney}!! \n \n Du vann!");
+                    Console.ReadLine();
                 }
                 else{
                     Console.WriteLine("Fel kod");
